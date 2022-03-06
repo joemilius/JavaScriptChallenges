9b1003ff621a1c540fd1b4ebd12bbe45441c3249
JoeMilius <joemilius@gmail.com>
2022-03-06 14:24:51 -0700

    slider places correct amount to screen in monetary value

--- PriceScale(6)/STARTER-FILES/app.js
+++ PriceScale(6)/STARTER-FILES/app.js
@@ -4,12 +4,17 @@ let price = document.querySelector('.amount .dollars')
 slider.addEventListener('input', (event) => {
     let priceString = event.target.value
     let priceValue = parseInt(event.target.value)
-    let dollars = priceString.slice(0, 2)
-    let cents = priceString.slice(2, 4)
-    if(priceValue === 0){
-    price.innerText = `0.00`
+
+    if(priceValue < 10){
+        price.innerText = `0.0${priceString.slice(0, 1)}`
+    }else if(priceValue < 100){
+        price.innerText = `0.${priceString.slice(0, 2)}` 
     }else if(priceValue < 1000){
-        price.innerText
+        price.innerText = `${priceString.slice(0,  1)}.${priceString.slice(1, 3)}`
+    }else if(priceValue < 10000){
+        price.innerText = `${priceString.slice(0,  2)}.${priceString.slice(2, 4)}`
+    }else if(priceValue >= 10000){
+        price.innerText = `${priceString.slice(0, 3)}.${priceString.slice(3, 5)}`
     }
     
 })
