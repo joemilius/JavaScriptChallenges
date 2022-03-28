e4de48108e983dc9ae41a0d8a663858b6113246e
JoeMilius <joemilius@gmail.com>
2022-03-27 23:51:24 -0600

    custom modalStarted

---
+++ Custom Modal(13)/STARTER-FILES/index.js
@@ -0,0 +1,10 @@
+let modal = document.querySelector('div.modal')
+let closeButton = document.querySelector('button.close')
+
+modal.addEventListener('click', event => {
+    console.log(event)
+})
+
+closeButton.addEventListener('click', event => {
+    console.log(event)
+})
