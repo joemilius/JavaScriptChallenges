fe69dd691c218f9dcff04afef2e2a8e97c3ff73e
JoeMilius <joemilius@gmail.com>
2022-03-28 22:12:39 -0600

    got the modal to hide

--- Custom Modal(13)/STARTER-FILES/index.js
+++ Custom Modal(13)/STARTER-FILES/index.js
@@ -1,10 +1,15 @@
+let overlay = document.querySelector('div.overlay')
 let modal = document.querySelector('div.modal')
 let closeButton = document.querySelector('button.close')
+console.log(closeButton)
 
-modal.addEventListener('click', event => {
-    console.log(event)
-})
+// modal.addEventListener('click', event => {
+//     console.log(event)
+    
+// })
 
 closeButton.addEventListener('click', event => {
-    console.log(event)
+    console.log(event.target)
+    overlay.style.visibility = 'hidden'
+    console.log(overlay)
 })
