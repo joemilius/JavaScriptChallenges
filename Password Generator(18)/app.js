e42834230d0c4eb7e3b3ce0b0c05f4d19df46e87
JoeMilius <joemilius@gmail.com>
2022-04-16 22:40:50 -0600

    aside scroll in table of contents established. inputs isolated for password generator

---
+++ Password Generator(18)/app.js
@@ -0,0 +1,8 @@
+let passwordInput = document.querySelector('#password')
+let lengthField = document.querySelctor('#length')
+let symbolCheck = document.querySelector('#symbols')
+let numbersCheck = document.querySelector('#numbers')
+let lowercaseCheck = document.querySelector('#lowercase')
+let uppercaseCheck = document.querySelector('#uppercase')
+let similarCheck = document.querySelector('#similar')
+
