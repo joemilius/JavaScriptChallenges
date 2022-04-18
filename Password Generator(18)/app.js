3b2680edad651a20579a78dc26ed488e4cdd5699
JoeMilius <joemilius@gmail.com>
2022-04-17 21:32:40 -0600

    default password generated when all boxes are checked and field is at 12

--- Password Generator(18)/app.js
+++ Password Generator(18)/app.js
@@ -1,8 +1,23 @@
 let passwordInput = document.querySelector('#password')
-let lengthField = document.querySelctor('#length')
+let lengthField = document.querySelector('#length')
 let symbolCheck = document.querySelector('#symbols')
 let numbersCheck = document.querySelector('#numbers')
 let lowercaseCheck = document.querySelector('#lowercase')
 let uppercaseCheck = document.querySelector('#uppercase')
 let similarCheck = document.querySelector('#similar')
 
+let generatorArray = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnpoqrstuvwxyz', '0123456789', '!?@#$%^&*<>-+=~']
+
+console.log(lengthField.value)
+
+passwordInput.value = generator()
+
+function generator(){
+    let password = ''
+    for(let i = 0; i < lengthField.value; i++){
+        let chosenString = generatorArray[Math.floor(Math.random() * generatorArray.length)]
+        password += chosenString.charAt(Math.floor(Math.random() * chosenString.length))
+    }
+    console.log(password)
+    return password 
+}
