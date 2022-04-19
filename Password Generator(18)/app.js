ef38b1c34e9e0081343ea5b3d14ab6df4f11676b
JoeMilius <joemilius@gmail.com>
2022-04-18 19:12:57 -0600

    length of password generated adjustable

--- Password Generator(18)/app.js
+++ Password Generator(18)/app.js
@@ -6,18 +6,32 @@ let lowercaseCheck = document.querySelector('#lowercase')
 let uppercaseCheck = document.querySelector('#uppercase')
 let similarCheck = document.querySelector('#similar')
 
-let generatorArray = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnpoqrstuvwxyz', '0123456789', '!?@#$%^&*<>-+=~']
+
+let keyArray = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnpoqrstuvwxyz', '0123456789', '!?@#$%^&*<>-+=~']
+let generatorArray = []
+
+let passwordLength = lengthField.value
 
 console.log(lengthField.value)
 
 passwordInput.value = generator()
 
+
 function generator(){
     let password = ''
-    for(let i = 0; i < lengthField.value; i++){
-        let chosenString = generatorArray[Math.floor(Math.random() * generatorArray.length)]
+    for(let i = 0; i < passwordLength; i++){
+        let chosenString = keyArray[Math.floor(Math.random() * generatorArray.length)]
+        console.log(chosenString)
         password += chosenString.charAt(Math.floor(Math.random() * chosenString.length))
     }
     console.log(password)
     return password 
 }
+
+lengthField.addEventListener('input',event => {
+    passwordLength = event.target.value
+    let lengthText = document.querySelector('#lengthText')
+    lengthText.innerText = event.target.value
+    passwordInput.value = generator()
+})
+//let lowercaseAndNumbers = Math.random().toString(36).slice(2, 7)
