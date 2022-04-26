99f20f2e9c6bb6c39e72d1be2bf2c9ba68495edc
JoeMilius <joemilius@gmail.com>
2022-04-26 15:03:34 -0600

    Form validator fully functional, just needs refactoring

--- Form Validator (19)/app.js
+++ Form Validator (19)/app.js
@@ -2,14 +2,15 @@ let nameInput = document.querySelector('#name')
 let emailInput = document.querySelector('#email')
 let passwordInput = document.querySelector('#password')
 let confirmPasswordInput = document.querySelector('#confirm-password')
+let passwordButtons = document.querySelectorAll('.show-hide')
+
 let passwordValue = ''
 
-console.log(nameInput.parentElement.children)
 emailInput.parentElement.children[2].innerHTML = '<img src="./images/error.svg" alt="Error" /> A valid email is required'
 emailInput.parentElement.children[3].innerHTML = ''
 
 nameInput.addEventListener('input', event => {
-    console.log(event.target.value)
+    
     if(event.target.value.length > 0){
         nameInput.parentElement.children[2].innerHTML = ''
         nameInput.parentElement.children[3].innerHTML = '<img src="./images/success.svg" alt="Success" />'
@@ -21,9 +22,7 @@ nameInput.addEventListener('input', event => {
 
 emailInput.addEventListener('input', event => {
     let value = event.target.value
-    console.log(value.slice(-4))
-    // console.log(value.includes('@yahoo.com'))
-    // console.log(value.includes('@comcast.net'))
+    
     if(value.includes('@') & value.slice(-4) === '.com' || value.includes('@') & value.slice(-4) === '.org'|| value.includes('@') & value.slice(-4) === '.net'){
         emailInput.parentElement.children[2].innerHTML = ''
         emailInput.parentElement.children[3].innerHTML = '<img src="./images/success.svg" alt="Success" />'
@@ -36,21 +35,33 @@ emailInput.addEventListener('input', event => {
 passwordInput.addEventListener('input', event => {
     if(event.target.value.length >= 8){
         passwordValue = event.target.value
-        passwordInput.parentElement.children[2].innerHTML = ''
-        passwordInput.parentElement.children[3].innerHTML = '<img src="./images/success.svg" alt="Success" />'
-    }else{
-        passwordInput.parentElement.children[2].innerHTML = '<img src="./images/error.svg" alt="Error" />'
         passwordInput.parentElement.children[3].innerHTML = ''
+        passwordInput.parentElement.children[4].innerHTML = '<img src="./images/success.svg" alt="Success" />'
+    }else{
+        passwordInput.parentElement.children[3].innerHTML = '<img src="./images/error.svg" alt="Error" /> Must be at least 8 characters'
+        passwordInput.parentElement.children[4].innerHTML = ''
     }
 })
 
 confirmPasswordInput.addEventListener('input', event => {
     if(event.target.value === passwordValue){
-        confirmPasswordInput.parentElement.children[2].innerHTML = ''
-        confirmPasswordInput.parentElement.children[3].innerHTML = '<img src="./images/success.svg" alt="Success" />'
-    }else{
-        confirmPasswordInput.parentElement.children[2].innerHTML = '<img src="./images/error.svg" alt="Error" /> Password does not match'
         confirmPasswordInput.parentElement.children[3].innerHTML = ''
+        confirmPasswordInput.parentElement.children[4].innerHTML = '<img src="./images/success.svg" alt="Success" />'
+    }else{
+        confirmPasswordInput.parentElement.children[3].innerHTML = '<img src="./images/error.svg" alt="Error" /> Password does not match'
+        confirmPasswordInput.parentElement.children[4].innerHTML = ''
     }
 })
 
+passwordButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        event.preventDefault()
+        if(event.target.parentElement.classList[1] === 'show'){
+            event.target.parentElement.classList.remove('show')
+            event.target.parentElement.children[0].type = 'password'
+        }else{
+            event.target.parentElement.classList.add('show')
+            event.target.parentElement.children[0].type = 'text'
+        }
+    })
+})
