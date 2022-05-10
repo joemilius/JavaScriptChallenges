0a4ef55f7bae6d7ab7eeab5a070a3e10a03870d7
JoeMilius <joemilius@gmail.com>
2022-05-09 19:45:02 -0600

    Credit Card Form: card flipped on cvv focus and blur, and credit card type changed based on credit card number

--- Credit Card Form (22)/app.js
+++ Credit Card Form (22)/app.js
@@ -3,13 +3,36 @@ let cardHolderInput = document.querySelector('[name="card-holder"]')
 let cvvInput = document.querySelector('[name="cvv"]')
 let expirationMonthSelect = document.querySelector('[name="expiration-date-month"]')
 let expirationYearSelect = document.querySelector('[name="expiration-date-year"]')
+let creditCardWrapper = document.querySelector('.credit-card__wrapper')
 
-cardNumberInput.addEventListener('input', event => {
+cardNumberInput.addEventListener('blur', event => {
     console.log(event.target.value)
+    let inputValue = event.target.value
+    if(inputValue.slice(0,2) === '34' || inputValue.slice(0,2) === '37'){
+        creditCardWrapper.classList.remove(creditCardWrapper.classList.value.split(' ')[1])
+        creditCardWrapper.classList.add('american')
+    }else if(inputValue.slice(0, 1) === '4'){
+        creditCardWrapper.classList.remove(creditCardWrapper.classList.value.split(' ')[1])
+        creditCardWrapper.classList.add('visa')
+    }else if(inputValue.slice(0, 1) === '5'){
+        creditCardWrapper.classList.remove(creditCardWrapper.classList.value.split(' ')[1])
+        creditCardWrapper.classList.add('mastercard')
+    }else if(inputValue.slice(0, 1) === '6'){
+        creditCardWrapper.classList.remove(creditCardWrapper.classList.value.split(' ')[1])
+        creditCardWrapper.classList.add('discover')
+    }
 })
 
 cardHolderInput.addEventListener('input', event => {
     console.log(event.target.value)
+})
+
+cvvInput.addEventListener('focus', event => {
+    creditCardWrapper.classList.add('flip')
+})
+
+cvvInput.addEventListener('blur', event => {
+    creditCardWrapper.classList.remove('flip')
 })
 
 cvvInput.addEventListener('input', event => {
