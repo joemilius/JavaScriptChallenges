a03bab8058191603c9beb7c5ceeb236fc7dbbc29
JoeMilius <joemilius@gmail.com>
2022-06-03 17:02:02 -0600

    Weather: rendered value for updated dates and forecast, still need icons and colors for the day

--- Weather-8/app.js
+++ Weather-8/app.js
@@ -5,7 +5,7 @@ const apiKey = config.WEATHER_API_KEY
 const daysOfWeekMap = {
     0: 'SUN', 
     1: 'MON', 
-    2: 'TUES', 
+    2: 'TUE', 
     3: 'WED', 
     4: 'THUR', 
     5: 'FRI', 
@@ -25,54 +25,81 @@ Find Weather Api
 Fetch the week's weather and update the html with values for the weather of each day of the week
 Fetch is currently only giving the first three days instead of the first 7 days
 */
-let today = 'SAT'
-//Date().split(' ')[0].toUpperCase()
-console.log(today)
-let day2 = ''
-let day3 = ''
+let today = Date().split(' ')[0].toUpperCase() === 'THU' ? 'THUR' : Date().split(' ')[0].toUpperCase()
 
-for(let key in daysOfWeekMap){
-    let second
-    let third
-    if(today === 'FRI'){
-        second = 6
-        third = 0
-        day2 = daysOfWeekMap[second]
-        day3 = daysOfWeekMap[third]
-    }else if(today === 'SAT'){
-        second = 0
-        third = 1
-        day2 = daysOfWeekMap[second]
-        day3 = daysOfWeekMap[third]
-    }else if(today === daysOfWeekMap[key]){
-        second = parseInt(key) + 1
-        third = parseInt(key) + 2
-        day2 = daysOfWeekMap[second]
-        day3 = daysOfWeekMap[third]
-        
-    }
-    console.log(day2, day3)
-}
+
 
 let wrapperDiv = document.querySelector('.wrapper')
-//wrapperDiv.innerHTML = ''
+wrapperDiv.innerHTML = ''
 let weather = []
 
-// function getForecast(){
-//     fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Denver&days=7&aqi=no&alerts=no`)
-//     .then(response => response.json())
-//     .then(data => {
-//         weather = data.forecast.forecastday
-//         weather.forEach(day => renderForecast(day))
-//     })
-// }
-// getForecast()
+function getForecast(){
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Denver&days=7&aqi=no&alerts=no`)
+    .then(response => response.json())
+    .then(data => {
+        weather = data.forecast.forecastday
+        console.log(weather)
+        weather.forEach(day => renderForecast(day))
+    })
+}
+getForecast()
 
 function renderForecast(day){
-    console.log(day)
+    let date = day.date
+    let dateArray = date.split('-')
+    dateArray[2] = date.split('-')[2].charAt(0) === '0' ? date.split('-')[2].charAt(1) : date.split('-')[2]
+    let newDate = Date(dateArray.join('-'))
+    let dayOfWeek = newDate.split(' ')[0].toUpperCase() === 'THU' ? 'THUR' : newDate.split(' ')[0].toUpperCase()
+    let dayOfMonth = date.split('-')[2].charAt(0) === '0' ? date.split('-')[2].charAt(1) : date.split('-')[2]
+    
     let dayDiv = document.createElement('div')
+    let dayOfWeekDiv = document.createElement('div')
+    let dateDiv = document.createElement('div')
+    let barDiv = document.createElement('div')
+    let weatherDiv = document.createElement('div')
+    let tempDiv = document.createElement('div')
+    let contentDiv = document.createElement('div')
+    let precipDiv = document.createElement('div')
+    let lowDiv = document.createElement('div')
+    let degreesSpan = document.createElement('span')
+
+    dayDiv.className = 'day'
+    dayOfWeekDiv.className = 'day-of-week'
+    dateDiv.className = 'date'
+    barDiv.className = 'bar'
+    weatherDiv.className = 'weather'
+    tempDiv.className = 'temperature'
+    contentDiv.className = 'content'
+    precipDiv.className = 'precipitation'
+    lowDiv.className = 'low'
+    degreesSpan.className = 'degrees'
+
+    dayOfWeekDiv.textContent = dayOfWeek
+    dateDiv.textContent = dayOfMonth
+    tempDiv.textContent = parseInt(day.day.maxtemp_f)
+    degreesSpan.innerHTML = '&deg;'
+    lowDiv.innerHTML = `<svg role="img" class="icon">
+                            <use xlink:href="#low"></use>
+                        </svg>
+                        ${parseInt(day.day.mintemp_f)}&deg;`
+    precipDiv.innerHTML = `<svg role="img" class="icon">
+                            <use xlink:href="#precipitation"></use>
+                         </svg>
+                         ${day.day.daily_chance_of_rain}%`
+
+    wrapperDiv.appendChild(dayDiv)
+    dayDiv.appendChild(dayOfWeekDiv)
+    dayDiv.appendChild(dateDiv)
+    dayDiv.appendChild(barDiv)
+    barDiv.appendChild(weatherDiv)
+    barDiv.appendChild(tempDiv)
+    barDiv.appendChild(contentDiv)
+    contentDiv.appendChild(precipDiv)
+    contentDiv.appendChild(lowDiv)
+    tempDiv.appendChild(degreesSpan)
 
 }
+
 
 
 
